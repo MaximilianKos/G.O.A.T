7dc72e619c28ba54b1f3c7ce625bb5ddd4d01e9f
MaximilianKos <maximiliankos.x@gmail.com>
2025-10-23 21:46:47 +0200

    lol theres stale changes

---
+++ frontend/src/app/components/bookmark-card/bookmark-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmark-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './bookmark-card.component.html',
+})
+export class BookmarkCardComponent {
+
+}
